0ad7b9686efd06667081a16cd7dbdca39d09c0da
jazbelrose <jazbelrose@icloud.com>
2023-10-18 01:30:14 -0700

    video play

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -131,7 +131,7 @@ export const Home = () => {
       </div>
       <div className="video-container">
       <video loop autoPlay muted>
-        <source src="https://mylgcontent.s3.us-west-1.amazonaws.com/Liquid+Bullets_RS+Camera_a.mp4" type="video/mp4" />
+        <source src="https://mylgcontent.s3.us-west-1.amazonaws.com/videos/liquid+bullet.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
