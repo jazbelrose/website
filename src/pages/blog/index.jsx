6f4a15c253059035c0c51b282ec2c8c03ae9799f
jazbelrose <jazbelrose@icloud.com>
2023-10-19 16:04:05 -0700

    Refactor Blog

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -3,55 +3,7 @@ import "./style.css";
 import { Link } from "react-router-dom";
 import ScrambleText from "scramble-text";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-
-
-const static_blog_posts = [
-  { id: 74782, title: "The Metaverse", slug: "the-metaverse" },
-  { id: 4252, title: "Content Strategy", slug: "content-strategy" },
-  {
-    id: 4239,
-    title: "花 Hana : Floral Design Essence",
-    slug: "earth-florals"
-  },
-  {
-    id: 4245,
-    title: "The Digital Marketing Future",
-    slug: "the-digital-marketing-future"
-  },
-  {
-    id: 4225,
-    title: "2023: Tech & Creativity",
-    slug: "2023-Tech-&-Creativity"
-  },
-  { id: 4636, title: "The Event Brainstorm", slug: "the-event-brainstorm" },
-  { id: 4233, title: "Nutrition & Art", slug: "nutrition-art" },
-  {
-    id: 20518,
-    title: "Building your personal brand.",
-    slug: "building-your-personal-brand"
-  },
-  { id: 20520, title: "Wabi-Sabi Design 侘寂", slug: "wabi-sabi-design" },
-  {
-    id: 1,
-    title: "Look into Lighting Design",
-    slug: "look-into-lighting-design"
-  },
-  {
-    id: 74783,
-    title: "Catering Art over Anything",
-    slug: "catering-art-over-anything"
-  },
-  { id: 74784, title: "The Print of 2023", slug: "the-print-of-2023" },
-  { id: 74785, title: "Textile Tones 2023", slug: "textile-tones-2023" },
-  {
-    id: 74786,
-    title: "Location Location Location",
-    slug: "location-location-location"
-  },
-  { id: 74787, title: "tīˈpäɡrəfē", slug: "typography" }, // changed to simple typography for slug
-  { id: 74788, title: "20/23 Vision", slug: "vision" },
-  { id: 74789, title: "The Art of Influence", slug: "the-art-of-influence" }
-];
+import allBlogPosts from './blogposts/allBlogPosts.json';  
 
 const BlogPost = ({ post }) => {
   const containerRef = useRef(null);
@@ -91,7 +43,7 @@ const BlogPost = ({ post }) => {
 };
 
 export const Blog = ({ maxPosts = 20 }) => {
-  const displayedPosts = static_blog_posts.slice(0, maxPosts);
+  const displayedPosts = allBlogPosts.slice(0, maxPosts);
   return (
     <HelmetProvider>
       <div className="container-fluid">
@@ -109,7 +61,7 @@ export const Blog = ({ maxPosts = 20 }) => {
               <BlogPost post={post} />
             </div>
           ))}
-          {maxPosts < static_blog_posts.length && (
+          {maxPosts < allBlogPosts.length && (
             <div className="reads-container">
               <a
                 href="https://belroselax.com/blog"
