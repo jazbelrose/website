487e3f46c657484513dd2a3c36fa6273b3437f19
jazbelrose <jazbelrose@icloud.com>
2023-11-24 16:54:54 -0800

    AOP DONE

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,8 +6,8 @@ import { meta } from "../../content_option";
 import { gsap } from "gsap";
 import { HeroSection } from "../../components/herosection";
 import { InfoSection } from "../../components/infosection";
-import { BlogEntry } from '../../components/blogentry';  
-import allBlogPosts from '../blog/allblogposts/allBlogPosts.json';  
+import { BlogEntry } from '../../components/blogentry';
+import allBlogPosts from '../blog/allblogposts/allBlogPosts.json';
 
 import PortfolioCard from "../../components/portfoliocard";
 
@@ -22,7 +22,7 @@ export const Home = () => {
     "L.A. +22 ← Paris, France +1  ←   New York. ←  London.  ←  California ",
     "ADPTV.TROIA.NOCCO.PD. BAREBELLS.MISTIFI.ZAPPOS.THE GOLD PRINCESS.MOKIBABY",
     "34.0549° N, 118.2426° W 48.8566° N, 2.3522° E 40.7128° N, 74.0060° W 51.5072° N, 0.1276° W"
-];
+  ];
 
   useEffect(() => {
     gsap.to(".bar", 1.5, {
@@ -43,202 +43,202 @@ export const Home = () => {
         <title>{meta.title} | Home </title>
         <meta name="description" content={meta.description} />
       </Helmet>
-     
-
-        <div class="overlay">
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-        </div>
 
 
-        <HeroSection />
+      <div class="overlay">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
 
 
-        <div className="work-header">
+      <HeroSection />
+
+
+      <div className="work-header">
         <h2>Work</h2>
         <span className="arrow-down work-arrow">↓</span>
-    </div>
+      </div>
 
 
-    
+
       <div className="portfolio-section">
-      
 
-        
+
+
         <div className="portfolio-row single-card-row">
-          <PortfolioCard 
+          <PortfolioCard
             className="single-card elf"
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Elf.jpg" 
-            imageAlt="e.l.f. Beauty Design" 
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Elf.jpg"
+            imageAlt="e.l.f. Beauty Design"
             title="e.l.f. Beauty"
-            subtitle="Nylon House Mokibaby Art Basel" 
-            description="3D Design, Immersive Digital" 
+            subtitle="Nylon House Mokibaby Art Basel"
+            description="3D Design, Immersive Digital"
           />
         </div>
-        
-        
+
+
         <div className="portfolio-row double-card-row">
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/The+Gold+Princess.jpg" 
-            imageAlt="The Gold Princess" 
-            title="The Gold Princess" 
-            subtitle="Royal Allure Campaign" 
-            description="Branding, Photography, Styling" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/The+Gold+Princess.jpg"
+            imageAlt="The Gold Princess"
+            title="The Gold Princess"
+            subtitle="Royal Allure Campaign"
+            description="Branding, Photography, Styling"
           />
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Bloom-And-Bliss.jpg" 
-            imageAlt="Bloom & Bliss Design" 
-            title="Bloom & Bliss" 
-            subtitle="Brand Identity" 
-            description="3D Animation, Branding Design" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Bloom-And-Bliss.jpg"
+            imageAlt="Bloom & Bliss Design"
+            title="Bloom & Bliss"
+            subtitle="Brand Identity"
+            description="3D Animation, Branding Design"
           />
         </div>
       </div>
 
       <div className="ticker-section">
 
-      <Ticker lines={tickerLines}  />
-</div>
- <div className="portfolio-section">
-        
+        <Ticker lines={tickerLines} />
+      </div>
+      <div className="portfolio-section">
+
         <div className="portfolio-row single-card-row">
-          <PortfolioCard 
+          <PortfolioCard
             className="single-card"
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Pipedream-Events.jpg" 
-            imageAlt="PD Events" 
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Pipedream-Events.jpg"
+            imageAlt="PD Events"
             title="PD Events"
-            subtitle="Branding & Web Design" 
-            description="3D Design, Animation, Web, Branding Design" 
+            subtitle="Branding & Web Design"
+            description="3D Design, Animation, Web, Branding Design"
           />
         </div>
         <div className="portfolio-row double-card-row">
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Chevron-strategy.jpg" 
-            imageAlt="Chevron" 
-            title="Chevron" 
-            subtitle="Turn it up 2023" 
-            description="3D Design, Immersive, Event Design" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Chevron-strategy.jpg"
+            imageAlt="Chevron"
+            title="Chevron"
+            subtitle="Turn it up 2023"
+            description="3D Design, Immersive, Event Design"
           />
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Jack-Masai.jpg" 
-            imageAlt="Jack Masaï" 
-            title="Jack Masaï" 
-            subtitle="StrikeFit Paris, France" 
-            description="Branding, Photography, Styling" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Jack-Masai.jpg"
+            imageAlt="Jack Masaï"
+            title="Jack Masaï"
+            subtitle="StrikeFit Paris, France"
+            description="Branding, Photography, Styling"
           />
         </div>
         <div className="portfolio-row single-card-row">
-          <PortfolioCard 
+          <PortfolioCard
             className="single-card gca"
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Ghost-Circus.jpg" 
-            imageAlt="Ghost Circus Apparel" 
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Ghost-Circus.jpg"
+            imageAlt="Ghost Circus Apparel"
             title="Ghost Circus Apparel"
-            subtitle="X by Eli James Collection" 
-            description="Branding, Photography, Web Design" 
+            subtitle="X by Eli James Collection"
+            description="Branding, Photography, Web Design"
           />
         </div>
       </div>
 
-      
+
       <div className="video-container">
-      <video loop autoPlay muted playsInline>
-        <source src="https://d2qb21tb4meex0.cloudfront.net/videos/liquid+bullet.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="portfolio-section">
-        
+        <video loop autoPlay muted playsInline>
+          <source src="https://d2qb21tb4meex0.cloudfront.net/videos/liquid+bullet.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="portfolio-section">
+
         <div className="portfolio-row single-card-row">
-          <PortfolioCard 
+          <PortfolioCard
             className="single-card nocco"
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Nocco+logo-01.png" 
-            imageAlt="NOCCO" 
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Nocco+logo-01.png"
+            imageAlt="NOCCO"
             title="NOCCO"
-            subtitle="Influencers Venice Beach, CA" 
-            description="3D Design, Immersive, Branding Design" 
+            subtitle="Influencers Venice Beach, CA"
+            description="3D Design, Immersive, Branding Design"
           />
         </div>
         <div className="portfolio-row double-card-row">
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/keys-soulcare.jpg" 
-            imageAlt="Keys Soulcare" 
-            title="Keys Soulcare" 
-            subtitle="Haller 6 Mokibaby" 
-            description="3D Design, Immersive, Digital" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/keys-soulcare.jpg"
+            imageAlt="Keys Soulcare"
+            title="Keys Soulcare"
+            subtitle="Haller 6 Mokibaby"
+            description="3D Design, Immersive, Digital"
           />
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Logitech.jpg" 
-            imageAlt="Logitech" 
-            title="Logitech" 
-            subtitle="Unleash your play" 
-            description="3D Design, Graphic Design" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/Logitech.jpg"
+            imageAlt="Logitech"
+            title="Logitech"
+            subtitle="Unleash your play"
+            description="3D Design, Graphic Design"
           />
         </div>
         <div className="portfolio-row double-card-row">
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/the-A.png" 
-            imageAlt="Academy of Pop" 
-            title="Academy of Pop" 
-            subtitle="Branding Mokibaby" 
-            description="3D Design, Immersive, Digital" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/the-A.png"
+            imageAlt="Academy of Pop"
+            title="Academy of Pop"
+            subtitle="Branding Mokibaby"
+            description="3D Design, Immersive, Digital"
           />
-          <PortfolioCard 
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/barebells.jpg" 
+          <PortfolioCard
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/barebells.jpg"
 
-            imageAlt="Barebells" 
-            title="Barebells" 
-            subtitle="Influencer Room" 
-            
-            description="3D Design, Graphic Design" 
+            imageAlt="Barebells"
+            title="Barebells"
+            subtitle="Influencer Room"
+
+            description="3D Design, Graphic Design"
           />
         </div>
-     
-     
+
+
 
         <div className="portfolio-row single-card-row">
-          <PortfolioCard 
+          <PortfolioCard
             className="single-card tcs"
-            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/the-chainsmokers.jpg" 
-            imageAlt="The Party Never Ends" 
+            imageSrc="https://d2qb21tb4meex0.cloudfront.net/images/the-chainsmokers.jpg"
+            imageAlt="The Party Never Ends"
             title="The Party Never Ends"
-            subtitle="Chainsmokers ADPTV" 
-            description="3D Design, Immersive, Event Design" 
+            subtitle="Chainsmokers ADPTV"
+            description="3D Design, Immersive, Event Design"
           />
         </div>
-     
-     
+
+
       </div>
 
-  <div className="footer-blog-section">
-      <div className="blog-header">
-        <h2>Blog</h2>
-        <span className="arrow-down blog-arrow">↓</span>
-    </div>
-    <div className="blog-grid">
-        {allBlogPosts.slice(0, 5).map((post, index) => (
+      <div className="footer-blog-section">
+        <div className="blog-header">
+          <h2>Blog</h2>
+          <span className="arrow-down blog-arrow">↓</span>
+        </div>
+        <div className="blog-grid">
+          {allBlogPosts.slice(0, 5).map((post, index) => (
             <BlogEntry key={index} post={post} />
-        ))}
-    </div>
-</div>
-<hr style={{ opacity: "1", color: "fff", height: "2px", backgroundColor: "#fff" }} />
-    
-    <InfoSection />
-    <hr style={{ opacity: "1", color: "fff", height: "2px", backgroundColor: "#fff", margin: "0.5rem", }} />
+          ))}
+        </div>
+      </div>
+      <hr style={{ opacity: "1", color: "fff", height: "2px", backgroundColor: "#fff" }} />
 
-    <div className="single-ticker-section">
+      <InfoSection />
+      <hr style={{ opacity: "1", color: "fff", height: "2px", backgroundColor: "#fff", margin: "0.5rem", }} />
 
-<SingleTicker />
-</div>
+      <div className="single-ticker-section">
 
-  
-  </HelmetProvider>
-);
+        <SingleTicker />
+      </div>
+
+
+    </HelmetProvider>
+  );
 };
--- src/pages/works/allworkposts/Academy-of-Pop.jsx
+++ src/pages/works/allworkposts/Academy-of-Pop.jsx
@@ -11,9 +11,13 @@ import { ReactComponent as Row2 } from "../../../assets/svg/Academy-of-Pop/row2.
 import { ReactComponent as Row3 } from "../../../assets/svg/Academy-of-Pop/row3.svg";
 import { ReactComponent as Row5 } from "../../../assets/svg/Academy-of-Pop/row5.svg";
 import { ReactComponent as Row6 } from "../../../assets/svg/Academy-of-Pop/row6.svg";
+import { ReactComponent as Row7 } from "../../../assets/svg/Academy-of-Pop/row7.svg";
+import { ReactComponent as Row8 } from "../../../assets/svg/Academy-of-Pop/row8.svg";
 
 import { ReactComponent as Asvg } from "../../../assets/svg/Academy-of-Pop/asvg.svg";
 import Ticker from "../../../components/ticker";
+import { InfoSection } from "../../../components/infosection";
+import SingleTicker from "../../../components/singleticker";
 
 import Counter from "../../../counter.jsx";
 
@@ -162,7 +166,7 @@ const AcademyOfPop = () => {
                     <div className="rendering-row-video">
                         {/* Left Element: Video */}
                         <div className="animation-container asvg-container">
-                        
+
                             <video width="100%" height="100%" loop autoPlay muted playsInline>
                                 <source src="https://mylgcontent.s3.us-west-1.amazonaws.com/02-Academy+of+Pop/00.mp4" type="video/mp4" />
                                 Your browser does not support the video tag.
@@ -170,7 +174,7 @@ const AcademyOfPop = () => {
 
                         </div>
 
-                        
+
                         <div className="right-content">
                             <Asvg className="asvg" />
                         </div>
@@ -202,9 +206,9 @@ const AcademyOfPop = () => {
                     <Row5 className="rendering-row" />
 
                     <div className="rendering-row-video">
-                        
+
                         <div className="aop-video-container">
-                        <video width="100%" height="100%" loop autoPlay muted playsInline>
+                            <video width="100%" height="100%" loop autoPlay muted playsInline>
                                 <source src="https://d2qb21tb4meex0.cloudfront.net/02-Academy+of+Pop/01.mp4" type="video/mp4" />
                                 Your browser does not support the video tag.
                             </video>
@@ -213,7 +217,7 @@ const AcademyOfPop = () => {
 
 
                         <div className="aop-video-container">
-                        <video width="100%" height="100%" loop autoPlay muted playsInline>
+                            <video width="100%" height="100%" loop autoPlay muted playsInline>
                                 <source src="https://d2qb21tb4meex0.cloudfront.net/02-Academy+of+Pop/02.mp4" type="video/mp4" />
                                 Your browser does not support the video tag.
                             </video>
@@ -223,18 +227,79 @@ const AcademyOfPop = () => {
 
                     <div className="rendering-row-video">
 
-                    <div className="aop-video-container 169">
-                    <video width="100%" height="100%" loop autoPlay muted playsInline>
+                        <div className="aop-video-container 169">
+                            <video width="100%" height="100%" loop autoPlay muted playsInline>
                                 <source src="https://mylgcontent.s3.us-west-1.amazonaws.com/02-Academy+of+Pop/03.mp4" type="video/mp4" />
                                 Your browser does not support the video tag.
                             </video>
 
                         </div>
+                    </div>
+
+                    <Row6 className="rendering-row" />
+                    <div className="rendering-top-row">
+                        <div className="rendering-header ll-studios">
+                            <h2>Interior  <br />
+                                Design</h2>
+                        </div>
+                        {/* <span className="arrow-down works-arrow">↓</span> */}
+                    </div>
+                    <div className="rendering-subheader ">
+                        <h3>LL Studios<br />
+                        </h3>
+                    </div>
+                    <Row7 className="rendering-row" />
+
+                    <div className="rendering-row-video">
+
+                        <div className="aop-video-container">
+                            <video width="100%" height="100%" loop autoPlay muted playsInline>
+                                <source src="https://mylgcontent.s3.us-west-1.amazonaws.com/02-Academy+of+Pop/04.mp4" type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+
                         </div>
 
-                        <Row6 className="rendering-row" />
-                        
 
+                        <div className="aop-video-container">
+                            <img src="https://mylgcontent.s3.us-west-1.amazonaws.com/02-Academy+of+Pop/20.png" alt="Academy of Pop Image" width="100%" height="100%" />
+                        </div>
+
+
+                    </div>
+
+                    <div className="rendering-layout">
+
+                        <Row8 className="rendering-row" />
+                    </div>
+
+
+                    <div className="aop-youtube-container">
+                        <iframe
+                            src="https://www.youtube.com/embed/qC-z4xPSOIA?modestbranding=1"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen>
+                        </iframe>
+                    </div>
+
+
+
+
+                </div>
+
+                <div className="rendering-infosection">
+
+                   
+
+                    <InfoSection />
+                    <hr style={{ opacity: "1", color: "fff", height: "2px", backgroundColor: "#fff", margin: "0.5rem", }} />
+
+                    <div className="single-ticker-section">
+
+                        <SingleTicker />
+                    </div>
                 </div>
 
             </div>
