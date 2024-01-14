0d10e844db712525fb876157a27e56b135f2a179
jazbelrose <jazbelrose@icloud.com>
2024-01-13 17:31:31 -0800

    reverse blog left-right

--- src/pages/blog/blogpost/BlogPost.js
+++ src/pages/blog/blogpost/BlogPost.js
@@ -1,15 +1,15 @@
 import React, { Suspense } from "react";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "./style.css";
-import allBlogPosts from '../allblogposts/allBlogPosts.json';
+import allBlogPosts from '../allblogposts/allBlogPosts.json';  
 import { NavigationDirectionContext } from "../../../app/contexts/NavigationDirectionProvider";
 import LeftArrow from "../../../assets/svg/chevron-left.svg";
 import RightArrow from "../../../assets/svg/chevron-right.svg";
 
 const BlogPost = () => {
 
-
-
+  
+  
   const { setDirection } = React.useContext(NavigationDirectionContext);
   const { postSlug } = useParams();
   const navigate = useNavigate();
@@ -31,41 +31,37 @@ const BlogPost = () => {
     });
   };
 
-
+  
 
   return (
     <div className="blog-content-wrapper">
       {previousPostSlug && (
-        <Link to={`/blog/${previousPostSlug}`}>
-          <div
-            className="arrow-container left"
-            onClick={() => handleArrowClick(previousPostSlug, "left")}
-          >
-            <img src={LeftArrow} alt="Previous Post" />
-          </div>
-        </Link>
+        <div
+          className="arrow-container left"
+          to={`/blog/${previousPostSlug}`}
+          onClick={() => handleArrowClick(previousPostSlug, "left")}
+        >
+          <img src={LeftArrow} alt="Previous Post" />
+        </div>
       )}
-  
+
       <Suspense fallback={<div>Loading...</div>}>
         <div className="blog-content-wrapper">
           <PostComponent />
         </div>
       </Suspense>
-  
+
       {nextPostSlug && (
-        <Link to={`/blog/${nextPostSlug}`}>
-          <div
-            className="arrow-container right"
-            onClick={() => handleArrowClick(nextPostSlug, "right")}
-          >
-            <img src={RightArrow} alt="Next Post" />
-          </div>
-        </Link>
+        <div
+          className="arrow-container right"
+          to={`/blog/${nextPostSlug}`}
+          onClick={() => handleArrowClick(nextPostSlug, "right")}
+        >
+          <img src={RightArrow} alt="Next Post" />
+        </div>
       )}
     </div>
   );
-    
-  
 };
 
 export default BlogPost;
