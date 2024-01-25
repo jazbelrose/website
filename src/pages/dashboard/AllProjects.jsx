31effdbf82e30c73d3b1a6f37228224834f79f8c
jazbelrose <jazbelrose@icloud.com>
2024-01-25 15:11:22 -0800

    Automatic all Thumbnail creation

--- src/pages/dashboard/AllProjects.jsx
+++ src/pages/dashboard/AllProjects.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Preloader from "../../components/preloader";
+import SVGThumbnail from './SVGThumbnail';
+import "./style.css";
 
 const AllProjects = ({ projects, onSelectProject, isLoading }) => {
     
@@ -7,16 +9,18 @@ const AllProjects = ({ projects, onSelectProject, isLoading }) => {
         return <Preloader />;
     }
 
-    // Once loading is complete, display the projects
     return (
         <div className='all-projects-container'>
             {projects.map(project => (
                 <div key={project.projectId} className="project-container" onClick={() => onSelectProject(project)}>
-                    <img src={project.thumbnails[0]} alt={`Thumbnail of ${project.title}`} className="project-thumbnail" />
-                    <h6 className="project-title">{project.title}</h6>
-                </div>
+    {project.thumbnails && project.thumbnails.length > 0 ?
+        <img src={project.thumbnails[0]} alt={`Thumbnail of ${project.title}`} className="project-thumbnail" /> :
+        <SVGThumbnail initial={project.title.charAt(0).toUpperCase()} className="project-thumbnail" />
+    }
+    <h6 className="project-title">{project.title}</h6>
+</div>
             ))}
-            </div>
+        </div>
     );
 };
 
---
+++ src/pages/dashboard/SVGThumbnail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SVGThumbnail = ({ initial, className }) => {
+    return (
+        <svg className={className} width="336" height="336" viewBox="0 0 336 336" xmlns="http://www.w3.org/2000/svg">
+            <text transform="translate(80 250)" style={{ fill: "#fff", fontFamily: "HelveticaNeueLT-Roman, 'HelveticaNeue LT 55 Roman'", fontSize: "241.23px" }}>
+                <tspan x="0" y="0">{initial}</tspan>
+            </text>
+            <rect x="7.41" y="7.07" width="321.17" height="321.86" rx="40" ry="40" style={{ fill: "none", stroke: "#fff", strokeMiterlimit: 10, strokeWidth: "11px" }}/>
+        </svg>
+    );
+};
+
+export default SVGThumbnail;
